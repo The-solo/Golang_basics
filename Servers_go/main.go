1b14df7ddc9e4f065a182c396eb9c1fdb6a1a891
The-solo <utkarshsase4321@gmail.com>
2026-03-12 13:51:03 +0530

    Learned about the custom http handlers

--- Servers_go/main.go
+++ Servers_go/main.go
@@ -10,18 +10,24 @@ func main(){
 
 	mux := http.NewServeMux() //Http request multiplexer
 
-	mux.Handle("/",http.FileServer(http.Dir("."))) // serving files from the current dir (index.html)
+	mux.Handle("/app/",http.StripPrefix("/app/", http.FileServer(http.Dir(".")))) // serving files from the current dir (index.html)
 
-	//standard http server struct.
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){ // this method satisfies the ServeHTTP interface
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // obvious headers.
+		w.WriteHeader(http.StatusOK)
+		fmt.Println("request reached the /healthz endpoint.")
+	})
+
 	port := "8080"
 	server := &http.Server{
 		Addr : ":"+port,
 		Handler : mux,
 	}
 	fmt.Println("The server is up & running on localhost:8080...")
-
+	
  	err := http.ListenAndServe(server.Addr, server.Handler)
 	if err != nil {
 		 fmt.Printf("Server connection failed", err)
 	}
+
 }
