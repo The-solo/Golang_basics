9429d77afbcef1d123ee6cce8bbefe15c4b0761a
The-solo <utkarshsase4321@gmail.com>
2026-03-17 19:18:23 +0530

    Introductions to middlwares

--- Servers_go/main.go
+++ Servers_go/main.go
@@ -4,30 +4,30 @@ import(
 	"io"
 	"fmt"
 	"net/http"
+	"server_basics.com/middlewares"
 )
-
 
 func main(){
 
-	mux := http.NewServeMux() //Http request multiplexer
+	router := http.NewServeMux() //Http request multiplexer
 
-	mux.Handle("/app/",http.StripPrefix("/app/", http.FileServer(http.Dir(".")))) // serving files from the current dir (index.html)
+	router.Handle("/app/",http.StripPrefix("/app/", http.FileServer(http.Dir(".")))) // serving files from the current dir (index.html)
 
 	// we write the response to ResponseWriter.
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){ // this method satisfies the http.Handler interface
+	router.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){ // this method satisfies the http.Handler interface
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // obvious headers.
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, "The server is up & ready to server.")
 	})
-
+	
 	port := "8080"
 	server := &http.Server{
 		Addr : ":"+port,
-		Handler : mux,
+		Handler : router,
 	}
-	fmt.Println("The server is up & running on port"+server.Addr)
+	fmt.Println("The server is up & running on port"+server.Addr+"....")
 	
- 	err := http.ListenAndServe(server.Addr, server.Handler)
+ 	err := http.ListenAndServe(server.Addr, middleware.MiddlewareLog(server.Handler))
 	if err != nil {
 		 fmt.Printf("Server connection failed", err)
 	}
